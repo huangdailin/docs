ee7209d1026282ccc1deee384646b67e43fce119
huangdailin <hdl25@qq.com>
2019-10-23 22:32:06 +0800

    增加看板和置顶功能

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,15 @@
 module.exports = {
   title: 'dailin‘s note', // 设置网站标题
   description: 'day day up', //描述
+  head: [
+     ['link', { rel: 'icon', href: '/head.ico' }],
+  ],
   //dest: './dist',   // 设置输出目录
   //port: 2333, //端口
   themeConfig: { //主题配置
    lastUpdated: '上传时间', 
+   // 定制文章标题颜色
+    accentColor: '#ac3e40',
     // 添加导航栏
     nav: [
       { text: '主页', link: '/' }, // 导航条
@@ -56,5 +61,40 @@ module.exports = {
     },
 	markdown: {
 		toc: { includeLevel: [2, 3] }
-	}
+	},
+	
+	plugins: [
+	[
+      'vuepress-plugin-gotop-plus', {
+        // 是否在移动设备上显示(default: true)
+        mobileShow: false,
+        // 回到页首元素显示触发的高度阈值(default: 50)
+        threshold: 50
+      }
+    ],
+    [
+      'vuepress-plugin-helper-live2d', {
+        live2d: {
+          // 是否启用(关闭请设置为false)(default: true)
+          enable: true,
+          // 模型名称(default: hibiki)>>>取值请参考：
+          // https://github.com/JoeyBling/hexo-theme-yilia-plus/wiki/live2d%E6%A8%A1%E5%9E%8B%E5%8C%85%E5%B1%95%E7%A4%BA
+          model: 'z16',
+          display: {
+            position: "right", // 显示位置：left/right(default: 'right')
+            width: 135, // 模型的长度(default: 135)
+            height: 300, // 模型的高度(default: 300)
+            hOffset: 65, //  水平偏移(default: 65)
+            vOffset: 0, //  垂直偏移(default: 0)
+          },
+          mobile: {
+            show: false // 是否在移动设备上显示(default: false)
+          },
+          react: {
+            opacity: 0.8 // 模型透明度(default: 0.8)
+          }
+        }
+      }
+    ],
+  ],
 }
